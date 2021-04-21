e8fa5eca1d47391b72ab447aec6879651be9a491
Renan Silva <renan.silva@ext.hdi.com.br>
2021-04-21 18:17:47 -0300

    feat: add Transaction creation endpoint

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,10 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface CreateTransactionRequest {
   title: string;
@@ -10,8 +14,40 @@ interface CreateTransactionRequest {
 }
 class CreateTransactionService {
   public async execute({ title, value, type, category }: CreateTransactionRequest): Promise<Transaction> {
-    // TODO
-    const;
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError(`Invalid transaction type: '${type}'`, 400);
+    }
+
+    let categoryInstance = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryInstance) {
+      // Caso não exista categoria, crio uma nova
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      categoryInstance = newCategory;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryInstance.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
